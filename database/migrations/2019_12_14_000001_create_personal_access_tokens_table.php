864f402b7fabe75632dcbbc0a603249c9b0ab50a
TP-O <letranphong20122001@gmail.com>
2022-01-13 15:29:04 +0700

    feat: update data tyepe in db

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -16,9 +16,9 @@ class CreatePersonalAccessTokensTable extends Migration
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
             $table->morphs('tokenable');
-            $table->string('name');
+            $table->string('name', 20);
             $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
+            $table->text('abilities')->default('[]');
             $table->timestamp('last_used_at')->nullable();
             $table->timestamps();
         });
